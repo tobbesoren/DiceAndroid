8039a4170c1fb6825a2cf5677e00625529cfe606
Tobias Sörensson <tobbesoren@gmail.com>
2022-11-03 13:34:58 +0100

    Now we have a six sided die image

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.properties.Delegates
 
@@ -13,17 +14,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val randomNumberText = findViewById<TextView>(R.id.randomNumberText)
+        //val randomNumberText = findViewById<TextView>(R.id.randomNumberText)
         val button = findViewById<Button>(R.id.button)
-        var numberOfSides : Int
+        //var numberOfSides : Int
+        val dieImageView = findViewById<ImageView>(R.id.dieImageView)
 
         button.setOnClickListener {
-            if(findViewById<EditText>(R.id.numberOfSidesEdit).text.toString().toIntOrNull() != null
+            /*if(findViewById<EditText>(R.id.numberOfSidesEdit).text.toString().toIntOrNull() != null
                 && findViewById<EditText>(R.id.numberOfSidesEdit).text.toString().toInt() > 0  ) {
                 numberOfSides = findViewById<EditText>(R.id.numberOfSidesEdit)
                     .text.toString().toInt()
                 randomNumberText.text = randomNumber(numberOfSides).toString()
-            }
+            }*/
+            rollDie(dieImageView)
 
         }
     }
@@ -31,5 +34,17 @@ class MainActivity : AppCompatActivity() {
         val randomNumber = (1..sides).random()
         Log.d("randomNumber: ", randomNumber.toString())
         return randomNumber
+    }
+
+    private fun rollDie(dieImageView : ImageView) {
+        val randomNumber = (1..6).random()
+        when(randomNumber) {
+            1 -> dieImageView.setImageResource(R.drawable.die1)
+            2 -> dieImageView.setImageResource(R.drawable.die2)
+            3 -> dieImageView.setImageResource(R.drawable.die3)
+            4 -> dieImageView.setImageResource(R.drawable.die4)
+            5 -> dieImageView.setImageResource(R.drawable.die5)
+            6 -> dieImageView.setImageResource(R.drawable.die6)
+        }
     }
 }
