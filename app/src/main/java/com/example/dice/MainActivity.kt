499e29c702769200f25b9895ef2ded656b2ded15
Tobias Sörensson <tobbesoren@gmail.com>
2022-11-03 13:59:15 +0100

    Guess hi or lo

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -4,10 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,19 +13,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //val randomNumberText = findViewById<TextView>(R.id.randomNumberText)
-        val button = findViewById<Button>(R.id.button)
+        val higherButton = findViewById<Button>(R.id.higherButton)
+        val lowerButton = findViewById<Button>(R.id.lowerButton)
         //var numberOfSides : Int
         val dieImageView = findViewById<ImageView>(R.id.dieImageView)
+        var lastRoll = 0
+        var score = 0
+        val currentScore = findViewById<TextView>(R.id.currentScoreText)
+        currentScore.text = score.toString()
 
-        button.setOnClickListener {
+        higherButton.setOnClickListener {
             /*if(findViewById<EditText>(R.id.numberOfSidesEdit).text.toString().toIntOrNull() != null
                 && findViewById<EditText>(R.id.numberOfSidesEdit).text.toString().toInt() > 0  ) {
                 numberOfSides = findViewById<EditText>(R.id.numberOfSidesEdit)
                     .text.toString().toInt()
                 randomNumberText.text = randomNumber(numberOfSides).toString()
             }*/
-            rollDie(dieImageView)
+            //rollDie(dieImageView)
+            val newRoll = rollDie(dieImageView)
+            if(newRoll > lastRoll) {
+                score++
+            } else {
+                score = 0
+            }
+            lastRoll = newRoll
+            currentScore.text = score.toString()
+        }
 
+        lowerButton.setOnClickListener {
+
+            val newRoll = rollDie(dieImageView)
+            if(newRoll < lastRoll) {
+                score++
+            } else {
+                score = 0
+            }
+            lastRoll = newRoll
+            currentScore.text = score.toString()
         }
     }
     private fun randomNumber(sides : Int): Int {
@@ -36,7 +58,7 @@ class MainActivity : AppCompatActivity() {
         return randomNumber
     }
 
-    private fun rollDie(dieImageView : ImageView) {
+    private fun rollDie(dieImageView : ImageView) : Int{
         val randomNumber = (1..6).random()
         when(randomNumber) {
             1 -> dieImageView.setImageResource(R.drawable.die1)
@@ -46,5 +68,6 @@ class MainActivity : AppCompatActivity() {
             5 -> dieImageView.setImageResource(R.drawable.die5)
             6 -> dieImageView.setImageResource(R.drawable.die6)
         }
+        return randomNumber
     }
 }
