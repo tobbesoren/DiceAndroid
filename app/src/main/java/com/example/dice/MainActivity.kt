cdc952b76bdf6a19873749482adcbd7a1c395ab5
Tobias Sörensson <tobbesoren@gmail.com>
2022-11-03 13:16:07 +0100

    Random number working

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.dice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val randomNumberText = findViewById<TextView>(R.id.randomNumberText)
+        val button = findViewById<Button>(R.id.button)
+        var numberOfSides : Int
+
+        button.setOnClickListener {
+            if(findViewById<EditText>(R.id.numberOfSidesEdit).text.toString().toIntOrNull() != null
+                && findViewById<EditText>(R.id.numberOfSidesEdit).text.toString().toInt() > 0  ) {
+                numberOfSides = findViewById<EditText>(R.id.numberOfSidesEdit)
+                    .text.toString().toInt()
+                randomNumberText.text = randomNumber(numberOfSides).toString()
+            }
+
+        }
+    }
+    private fun randomNumber(sides : Int): Int {
+        val randomNumber = (1..sides).random()
+        Log.d("randomNumber: ", randomNumber.toString())
+        return randomNumber
     }
 }
